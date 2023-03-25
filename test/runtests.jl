48074bb858f8cafb9e923a68b23ceccf7aac32fc
miguelraz <miguelraz@ciencias.unam.mx>
2023-03-24 18:20:30 -0600

    setup testing infra

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,8 @@ APoint = [
 
 
 
-inputs = [[parse(Float64, x) for x in eachline(s)] for s in APoint]
-@test length(inputs) == 10
+APoint_inputs = [[parse(Float64, x) for x in eachline(s)] for s in APoint]
+@test length(APoint_inputs) == 10
 
 Sites = [
     "Sites_Test_1.csv"
@@ -34,18 +34,25 @@ Sites = [
     "Sites_Test_10.csv"
 ]
 
-outputs = [[parse.(BigFloat, split(s, ',')) for s in eachline(x)] for x in Sites]
+Sites_outputs = [[parse.(BigFloat, split(s, ',')) for s in eachline(x)] for x in Sites]
 
-@test length(outputs) == 10
+@test length(Sites_outputs) == 10
+@test length(Sites_outputs[1]) == 4897
+@test length(Sites_outputs[2]) == 4880
+@test length(Sites_outputs[3]) == 4917
+@test length(Sites_outputs[4]) == 4897
+@test length(Sites_outputs[5]) == 4880
+@test length(Sites_outputs[6]) == 4902
+@test length(Sites_outputs[7]) == 4894
+@test length(Sites_outputs[8]) == 4913
+@test length(Sites_outputs[9]) == 4899
+@test length(Sites_outputs[10]) == 4889
 
 @testset "QuasiPeriodicCrystals.jl" begin
     # Write your tests here.
     @test dot([1, 2], [3, 4]) == 11
     @test dot_Product([1, 2], [3, 4]) == 11
     @test orthogonal_Vec([1, 2]) == (2, -1)
-    for _ in 1:100
-        @test hypot.(arb_Point(3.0)...) <= 3√2
-    end
 end
 
 
@@ -87,13 +94,13 @@ writedlm("Sites_Test_N$(NSides).csv", MainClusterSites, ',');
 # Test 3
 
 =#
-RTests_inputs = [
+RTests_outputs = [
     "Sites_Test_N5.csv"
     "Sites_Test_N8.csv"
     "Sites_Test_N23.csv"
 ]
 
-RTests_outputs = [[parse.(BigFloat, split(s, ',')) for s in eachline(x)] for x in RTests_inputs]
+RTests_outputs = [[parse.(BigFloat, split(s, ',')) for s in eachline(x)] for x in RTests_outputs]
 @test length(RTests_outputs) == 3
 @test length(RTests_outputs[1]) == 4739
 @test length(RTests_outputs[2]) == 4651
